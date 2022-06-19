b785b78418f9e570cf4fac266374d9e8d19b7471
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-19 14:33:41 +0530

    Add mobile response button

--- navbar/src/components/navbar/Navbar.js
+++ navbar/src/components/navbar/Navbar.js
@@ -1,33 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 
 import { Link } from "react-router-dom";
+import { FaBars } from 'react-icons/fa'
+import { ImCross } from 'react-icons/fa'
+
 
 const Navbar = () => {
+
+  const [Mobile,setMobile]=useState(false)
+
   return (
     <nav className="navbar">
-      <h3 className="logo">Logo</h3>
+      <div className="container">
+        <h3 className="logo">Logo</h3>
 
-      <ul className="nav-links">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/dashboard">
-          <li>Dashboard</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-        <Link to="/service">
-          <li>Service</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact</li>
-        </Link>
-        <Link to="/profile">
-          <li>Profile</li>
-        </Link>
-      </ul>
+        <ul className="nav-links">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/dashboard">
+            <li>Dashboard</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/service">
+            <li>Service</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+        </ul>
+        <button className='mobile-menu'>
+          {Mobile? <ImCross />}
+          <FaBars />
+        </button>
+      </div>
     </nav>
   );
 }
