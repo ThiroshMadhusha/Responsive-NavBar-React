3cef2b8e6c814be893ff14fce8158ff674f4e439
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-19 13:55:26 +0530

    Implement navbar

--- navbar/src/components/navbar/Navbar.js
+++ navbar/src/components/navbar/Navbar.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import './navbar.css'
+
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <h3 className='logo'>Logo</h3>
-      
+    <nav className="navbar">
+      <h3 className="logo">Logo</h3>
+
+      <ul className="nav-links">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/dashboard">
+          <li>Dashboard</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/service">
+          <li>Service</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
+        <Link to="/profile">
+          <li>Profile</li>
+        </Link>
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar
